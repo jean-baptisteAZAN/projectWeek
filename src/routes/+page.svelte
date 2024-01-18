0b8ba2936bd668e9774d0fdc779807a65c54828b
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-01-18 11:16:14 +0100

    add: Droid.cpp && Droid.hpp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,14 +21,8 @@
                         placeholder="Nom de votre goute" />
             </div>
         </div>
-        <button on:click={enterSite} class="relative inline-flex group">
-            <span
-                    class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
-            </span>
-            <button title="Get quote now"
-               class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
-                Entrer
-            </button>
+        <button on:click={enterSite} class=" px-10 py-5 bg-gradient-to-r from-blue-800 to-blue-400 text-white rounded-3xl text-3xl">
+            Entrer
         </button>
     </div>
 {:else}
